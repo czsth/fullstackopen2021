2fe47ed5b896a42f6d7c77fbb22dc9ba15ebbc24
czsth <czcooper@outlook.com>
2021-12-29 10:13:30 +0800

    exercise 2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ query, onQueryChange }) => {
     return (
@@ -66,22 +67,27 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
-
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                console.log(`${response.data.length} person(s) got`)
+                setPersons(response.data)
+            })
+    }, [])
 
     const addPerson = (event) => {
         event.preventDefault()
 
         if (persons.map(person => person.name).includes(newName)) {
-            alert(`${newName} is already added to phonebook`)
+            alert(`${newName} is already added to phone book`)
         } else {
             const personObject = {
                 name: newName,
@@ -113,7 +119,7 @@ const App = () => {
 
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h2>Phone Book</h2>
 
             <Filter
                 query={query}
