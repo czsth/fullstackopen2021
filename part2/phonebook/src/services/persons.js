e7d694ec982f55ab86f8d75b5055b62cf00bac76
czsth <czcooper@outlook.com>
2022-01-02 12:07:00 +0800

    handle warning import/no-anonymous-default-export

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -21,4 +21,6 @@ const update = (id, newObject) => {
     return request.then(response => response.data)
 }
 
-export default { getAll, create, remove, update }
+const personService = { getAll, create, remove, update }
+
+export default personService
